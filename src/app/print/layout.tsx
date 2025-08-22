40240f1d0ce303f9a87ebd991a26ad76df924002
Innovative Euforia Enterprises <ieenterprises.ai@gmail.com>
2025-08-22 17:34:34 +0000

    My Web Application is Working Perfectly well do not modify or change any

--- src/app/print/layout.tsx
+++ src/app/print/layout.tsx
@@ -1,14 +1,12 @@
 
-import { SettingsProvider } from "@/hooks/use-settings";
-
 export default function PrintLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <SettingsProvider>
+    <>
         {children}
-    </SettingsProvider>
+    </>
   );
 }
